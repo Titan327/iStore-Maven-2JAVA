8a27617b6009912c5983fe060863d69dc827c604
Titan327 <tristan.tourbier@gmail.com>
2023-01-29 13:21:52 +0100

    Projet configurer

--- src/main/java/iStore_ltd/Main.java
+++ src/main/java/iStore_ltd/Main.java
@@ -1,7 +1,15 @@
 package iStore_ltd;
 
+import java.util.ArrayList;
+import java. util.List;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String [] args){
+        List<String>name = new ArrayList<String>();
+        name.add("daniel");
+        name.add("bob");
+        name.add("Mia");
+        name.forEach(n->System.out.println(n));
+
     }
+
 }
