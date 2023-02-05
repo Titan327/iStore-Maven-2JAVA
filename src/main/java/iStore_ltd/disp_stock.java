d6e67ce2a952bac3e3d2d8f2a61f29812f7d2c70
Titan327 <tristan.tourbier@gmail.com>
2023-02-06 00:50:26 +0100

    update

--- src/main/java/iStore_ltd/disp_stock.java
+++ src/main/java/iStore_ltd/disp_stock.java
@@ -29,12 +29,13 @@ public class disp_stock {
 
     public disp_stock(String email) {
 
-        System.out.println(email);
+        //System.out.println(email);
 
         tab_data.setModel(new DefaultTableModel(
                 null,
-                new String[]{"nom du produit","quantité","prix"}
+                new String[]{"nom du produit","quantité","prix/unité"}
         ));
+
 
 
         connection_DB db = connection_DB.getInstance();
@@ -44,14 +45,40 @@ public class disp_stock {
         try {
 
             Statement statement1 = connection.createStatement();
-            ResultSet result1 = statement1.executeQuery("SELECT name FROM product;");
+            ResultSet result1 = statement1.executeQuery("SELECT name,id,price FROM product;");
+            String[] list = {};
+            String[] list_name_product = {};
+            String[] list_price_product = {};
+
             while (result1.next()) {
 
                 String name_product = result1.getString("name");
+                String product_id = result1.getString("id");
+                String product_price = result1.getString("price");
                 comb_obj.addItem(name_product);
+
+                String[] new_list = new String[list.length + 1];
+                System.arraycopy(list, 0, new_list, 0, list.length);
+                new_list[new_list.length - 1] = product_id;
+                list = new_list;
+
+                String[] new_list_name_product = new String[list_name_product.length + 1];
+                System.arraycopy(list_name_product, 0, new_list_name_product, 0, list_name_product.length);
+                new_list_name_product[new_list_name_product.length - 1] = name_product;
+                list_name_product = new_list_name_product;
+
+                String[] new_list_price_product = new String[list_price_product.length + 1];
+                System.arraycopy(list_price_product, 0, new_list_price_product, 0, list_price_product.length);
+                new_list_price_product[new_list_price_product.length - 1] = product_price;
+                list_price_product = new_list_price_product;
 
             }
 
+            /*
+            for (int i = 0; i < list.length; i++) {
+                System.out.println(list[i]);
+            }
+            */
 
             PreparedStatement statement2 = connection.prepareStatement("SELECT email_id, pseudo FROM users WHERE email_id = (SELECT id FROM whitelist WHERE email = ?);");
             statement2.setString(1, email);
@@ -72,7 +99,9 @@ public class disp_stock {
 
             }
 
-            PreparedStatement statement4 = connection.prepareStatement("SELECT product.name, product_in_store.quantity, product.price FROM product JOIN product_in_store ON product.id = product_in_store.product_id WHERE store_id = ?;");
+            String[] list2 = {};
+
+            PreparedStatement statement4 = connection.prepareStatement("SELECT product.name, product_in_store.quantity, product.price, product.id FROM product JOIN product_in_store ON product.id = product_in_store.product_id WHERE store_id = ?;");
             statement4.setString(1, String.valueOf(store_id));
             ResultSet result4 = statement4.executeQuery();
             while (result4.next()) {
@@ -80,13 +109,47 @@ public class disp_stock {
                 String name_product = result4.getString("product.name");
                 int quantity_product = result4.getInt("product_in_store.quantity");
                 int product_price = result4.getInt("product.price");
+                String product_id = result4.getString("product.id");
 
                 DefaultTableModel model = (DefaultTableModel) tab_data.getModel();
                 Object[] data = {name_product, quantity_product,product_price};
                 model.addRow(data);
 
+                String[] new_list2 = new String[list2.length + 1];
+                System.arraycopy(list2, 0, new_list2, 0, list2.length);
+                new_list2[new_list2.length - 1] = product_id;
+                list2 = new_list2;
+
 
             }
+
+            boolean find = false;
+            for (int i = 0; i < list.length; i++) {
+                find = false;
+                //System.out.println(list[i]);
+                for (int j = 0; j < list2.length; j++) {
+                    //System.out.println(list2[j]);
+                    if(list[i].equals(list2[j])){
+                        find = true;
+                        break;
+                    }
+                }
+                if(find){
+                    System.out.println("trouvé");
+                }
+                else {
+                    PreparedStatement statement5 = connection.prepareStatement("INSERT INTO product_in_store (product_id, store_id, quantity) VALUES (?,?,?);");
+                    statement5.setString(1, list[i]);
+                    statement5.setString(2, String.valueOf(store_id));
+                    statement5.setString(3, "0");
+                    statement5.executeUpdate();
+
+                    DefaultTableModel model = (DefaultTableModel) tab_data.getModel();
+                    Object[] data = {list_name_product[i], 0,list_price_product[i]};
+                    model.addRow(data);
+                }
+            }
+
 
         }
         catch (SQLException exp) {
@@ -112,54 +175,68 @@ public class disp_stock {
                 try {
 
                     String product = String.valueOf(comb_obj.getSelectedItem());
+                    String product_name = null;
+                    String product_price = null;
 
-                    PreparedStatement statement = connection.prepareStatement("SELECT id FROM product WHERE name = ?;");
+                    PreparedStatement statement = connection.prepareStatement("SELECT name,id,price FROM product WHERE name = ?;");
                     statement.setString(1, product);
                     ResultSet result = statement.executeQuery();
                     while (result.next()) {
 
                         product = result.getString("id");
+                        product_name = result.getString("name");
+                        product_price = result.getString("price");
+
+                        System.out.println("product from db" + product);
 
                     }
+
 
 
                     String quantity = tf_quanti_sell.getText();
 
-                    int product_id = comb_obj.getSelectedIndex();
-                    Object value = tab_data.getValueAt(product_id, 1);
+                    //System.out.println(tab_data.getRowCount());
 
-                    String str_val = String.valueOf(value);
+                    for (int i = 0; i < tab_data.getRowCount() ; i++) {
 
-                    int value_int = 0;
+                        if(String.valueOf(tab_data.getValueAt(i, 0)).equals(product_name)){
 
-                    if(comb_sell_add.getSelectedIndex() == 0) {
+                            int product_id = comb_obj.getSelectedIndex();
+                            String str_val = String.valueOf(tab_data.getValueAt(product_id, 1));
 
-                        value_int = Integer.parseInt(str_val) - Integer.parseInt(quantity);
+                            int value_int = 0;
 
-                        if(value_int < 0 ){
+                            if(comb_sell_add.getSelectedIndex() == 0) {
 
-                            JOptionPane.showMessageDialog(frame, "Erreur : Stock insuffisant pour cette action", "Erreur", JOptionPane.ERROR_MESSAGE);
+                                value_int = Integer.parseInt(str_val) - Integer.parseInt(quantity);
+
+                                if(value_int < 0 ){
+
+                                    JOptionPane.showMessageDialog(frame, "Erreur : Stock insuffisant pour cette action", "Erreur", JOptionPane.ERROR_MESSAGE);
+                                    return;
+
+                                }
+
+                            } else if (comb_sell_add.getSelectedIndex() == 1) {
+
+                                value_int = Integer.parseInt(str_val) + Integer.parseInt(quantity);
+
+                            }
+
+                            DefaultTableModel model = (DefaultTableModel) tab_data.getModel();
+                            model.setValueAt(value_int, product_id, 1);
+
+                            PreparedStatement statement2 = connection.prepareStatement("UPDATE product_in_store SET quantity = ? WHERE product_id = ? AND store_id = ?;");
+
+                            statement2.setString(1, String.valueOf(value_int));
+                            statement2.setString(2, product);
+                            statement2.setString(3, String.valueOf(store_id));
+                            statement2.executeUpdate();
+
                             return;
 
                         }
-
-                    } else if (comb_sell_add.getSelectedIndex() == 1) {
-
-                        value_int = Integer.parseInt(str_val) + Integer.parseInt(quantity);
-
                     }
-
-                    DefaultTableModel model = (DefaultTableModel) tab_data.getModel();
-                    model.setValueAt(value_int, product_id, 1);
-
-                    PreparedStatement statement2 = connection.prepareStatement("UPDATE product_in_store SET quantity = ? WHERE product_id = ? AND store_id = ?;");
-
-                    statement2.setString(1, String.valueOf(value_int));
-                    statement2.setString(2, product);
-                    statement2.setString(3, String.valueOf(store_id));
-                    statement2.executeUpdate();
-
-
                 }
                 catch(SQLException exp){
                         exp.printStackTrace();
--- src/main/java/iStore_ltd/test.java
+++ src/main/java/iStore_ltd/test.java
@@ -1,21 +1,12 @@
 package iStore_ltd;
 
-import javax.swing.*;
 
 public class test {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Tableau Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        String[] columns = {"Colonne 1", "Colonne 2", "Colonne 3"};
-        Object[][] data = {{"Donnée 1,1", "Donnée 1,2", "Donnée 1,3"},
-                {"Donnée 2,1", "Donnée 2,2", "Donnée 2,3"},
-                {"Donnée 3,1", "Donnée 3,2", "Donnée 3,3"}};
-        JTable table = new JTable(data, columns);
-        JScrollPane scrollPane = new JScrollPane(table);
-        frame.add(scrollPane);
-        frame.setSize(450, 200);
-        frame.setVisible(true);
-
-        System.out.println("test");
+        int[] numbers = {1, 2, 3};
+        int[] newNumbers = new int[numbers.length + 1];
+        System.arraycopy(numbers, 0, newNumbers, 0, numbers.length);
+        newNumbers[newNumbers.length - 1] = 4;
+        System.out.println(newNumbers);
     }
 }
