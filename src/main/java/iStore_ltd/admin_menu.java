aba200140d3fbbc32e80a1f804873aa895e16d89
Titan327 <tristan.tourbier@gmail.com>
2023-02-08 15:36:13 +0100

    update

--- src/main/java/iStore_ltd/admin_menu.java
+++ src/main/java/iStore_ltd/admin_menu.java
@@ -4,4 +4,34 @@ import javax.swing.*;
 
 public class admin_menu {
     private JLabel lb_logo;
+    private JLabel lb_search_user;
+    private JLabel lb_whitelist;
+    private JLabel lb_pseudo_search_admin;
+    private JTextField tf_pseudo_search_admin;
+    private JLabel lb_email_search;
+    private JTextField tf_email;
+    private JLabel lb_pseudo_search;
+    private JTextField tf_pseudo;
+    private JLabel lb_role_search;
+    private JComboBox cb_role;
+    private JButton b_val_modif;
+    private JTextField tf_email_whitelist;
+    private JLabel lb_email_whitelist;
+    private JButton b_add_whitelist;
+    private JLabel lb_store_list;
+    private JTable table1;
+    private JLabel lb_del_store;
+    private JButton b_del_store;
+    private JLabel lb_add_store;
+    private JTextField tf_store_name;
+    private JButton b_add_store;
+    private JTable t_product;
+    private JScrollPane t_shop;
+    private JComboBox cb_nom_store_del;
+    private JComboBox comboBox1;
+    private JButton b_del_product;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton b_add_product;
+    private JButton b_search;
 }
